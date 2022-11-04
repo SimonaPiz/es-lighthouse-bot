b655cd519c21572103977c47ebf0841a3f11d6f2
Simona Pizio <simona.pizio@gmail.com>
2022-11-04 13:22:27 +0100

    Change config CI and lighthouse

--- lighthouserc.js
+++ lighthouserc.js
@@ -1,10 +1,21 @@
 module.exports = {
-  extends: 'lighthouse:default',
-  settings: {
-    onlyAudits: [
-      'first-meaningful-paint',
-      'speed-index',
-      'interactive',
-    ],
+  ci: {
+    collect: {
+      /* Add configuration here */
+      staticDistDir: './public',
+      startServerCommand: 'npm run start',
+      url: ['http://localhost:8080'],
+      numberOfRuns: 3
+    },
+    assert: {
+      assertions: {
+        'categories:performance': ['warn', {minScore: 1}],
+        'categories:accessibility': ['error', {minScore: 1}]
+      }
+    },
+    upload: {
+      /* Add configuration here */
+      target: 'temporary-public-storage',
+    },
   },
 };
